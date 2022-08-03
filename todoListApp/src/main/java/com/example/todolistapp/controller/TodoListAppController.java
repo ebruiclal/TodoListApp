b411b41b1423f6a809de2465feaa77692d4b4bfe
Ebru İclal <ebruiclaldilercan@gmail.com>
2022-08-03 14:03:47 +0300

    List

---
+++ todoListApp/src/main/java/com/example/todolistapp/controller/TodoListAppController.java
@@ -0,0 +1,26 @@
+package com.example.todolistapp.controller;
+
+import com.example.todolistapp.model.Todo;
+import com.example.todolistapp.model.TodoRepository;
+import com.example.todolistapp.model.User;
+import com.example.todolistapp.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoListAppController {
+    @Autowired
+    TodoRepository todoRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping
+    public Iterable<Todo> todos(){
+        return todoRepository.findAll();
+    }
+    @GetMapping("/users")
+    public Iterable<User> users(){
+        return userRepository.findAll();
+    }
+}
